8ecb2cb59638bfd7599fc8faff2b7dd08bd8bec4
Artem Petrov <artem343p@gmail.com>
2020-04-22 10:47:37 +0300

    Add number of unapproved cmuxes

--- app/cmux_project/settings/base.py
+++ app/cmux_project/settings/base.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cmuxovik.context_processors.number_of_unapproved',
             ],
         },
     },
